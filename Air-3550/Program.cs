64f8a4178a9d8f735d865aded7ac37f7fd71521f
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-04-03 14:28:22 -0400

    basic query

--- Air-3550/Program.cs
+++ Air-3550/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Air_3550
 {
@@ -7,6 +8,18 @@ namespace Air_3550
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            using (var db = new AirContext())
+            {
+                Console.WriteLine("Querying Airports");
+                var toledoExpress = db.Airports.Where(a => a.City == "Toledo").First();
+                Console.WriteLine(toledoExpress.City);
+                Console.WriteLine(toledoExpress.State);
+                Console.WriteLine(toledoExpress.Country);
+                Console.WriteLine(toledoExpress.Latitude);
+                Console.WriteLine(toledoExpress.Longitude);
+                Console.WriteLine(toledoExpress.Elevation);
+            }
+            
         }
     }
 }
