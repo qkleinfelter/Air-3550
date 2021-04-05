60823824c564c1a10f53d9c501f0a5acccd81a5d
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-04-05 10:30:30 -0400

    some shit that doesn't work but should

--- Air-3550/AirContext.cs
+++ Air-3550/AirContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Air_3550
@@ -18,11 +19,30 @@ namespace Air_3550
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
+            Console.WriteLine("Entered OnConfiguring");
             var appDataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             var airDir = Path.Combine(appDataDirectory, "Air 3550 Team 11");
             Directory.CreateDirectory(airDir);
             var dbPath = Path.Combine(airDir, "air3550.db");
             options.UseSqlite(@"Data Source=" + dbPath);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Console.WriteLine("Entered OnModelCreating");
+            modelBuilder.Entity<Airport>().HasData(
+                new Airport
+                {
+                    AirportId = 1,
+                    AirportCode = "TOL",
+                    City = "Toledo",
+                    State = "Ohio",
+                    Country = "USA",
+                    Latitude = 41.586806M,
+                    Longitude = -83.807833M,
+                    Elevation = 684
+                }
+                );
         }
     }
 }
