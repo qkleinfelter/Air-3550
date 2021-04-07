fc60a0790f6e0c5408bc3714c76da080f2fca574
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-04-07 19:30:55 -0400

    more seeding

---
+++ Database/Migrations/20210407232741_SeedPlanesAndStaff.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class SeedPlanesAndStaff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Planes",
+                columns: new[] { "PlaneId", "MaxDistance", "MaxSeats", "Model" },
+                values: new object[] { 1, 6570, 230, "Boeing 737 MAX" });
+
+            migrationBuilder.InsertData(
+                table: "Planes",
+                columns: new[] { "PlaneId", "MaxDistance", "MaxSeats", "Model" },
+                values: new object[] { 2, 14815, 416, "Boeing 747" });
+
+            migrationBuilder.InsertData(
+                table: "Planes",
+                columns: new[] { "PlaneId", "MaxDistance", "MaxSeats", "Model" },
+                values: new object[] { 3, 6241, 199, "Boeing 757" });
+
+            migrationBuilder.InsertData(
+                table: "Planes",
+                columns: new[] { "PlaneId", "MaxDistance", "MaxSeats", "Model" },
+                values: new object[] { 4, 17395, 550, "Boeing 777" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "HashedPass", "LoginId", "UserRole" },
+                values: new object[] { 1, "360caebd9edb68609c0933bade3565350e59e284cc503ce61bf0eebd42fb7e5bd657a71ed1498225168757e7f1095920411cce27779e0c778ec52535deae2040", "marketing_manager", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "HashedPass", "LoginId", "UserRole" },
+                values: new object[] { 2, "41ec260efa3aa054a91cc6cf9441e3652637f75946c8fa6c2e926f289e095f46e62e19a28e02fb0fc25dd047b1f04e6e03cf930d464b540c40c0045eb6b7e252", "load_engineer", 2 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "HashedPass", "LoginId", "UserRole" },
+                values: new object[] { 3, "ab8f196b4521a3aba1de420fe6ef552ce406d8551c3aef370f909cea85abbc77ca1d698ef31f659097eee16e365975047ff403df1aae6cc7ef54595c3ae4d172", "accounting_manager", 3 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "HashedPass", "LoginId", "UserRole" },
+                values: new object[] { 4, "e2289f3f3a66a81f5ffb52dff1a09cd2ae91a39eb248230d2907084679c6bacdb4a880da7835ca72003d8c37e107cf91c5f9795678303754eba1be42039bff4d", "flight_manager", 4 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Planes",
+                keyColumn: "PlaneId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Planes",
+                keyColumn: "PlaneId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Planes",
+                keyColumn: "PlaneId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Planes",
+                keyColumn: "PlaneId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 4);
+        }
+    }
+}
--- Database/Repo/AirContext.cs
+++ Database/Repo/AirContext.cs
@@ -32,6 +32,8 @@ namespace Air_3550.Repo
             Console.WriteLine("Entered OnModelCreating");
 
             // Seeding airport data
+            // note: Elevation is in units of feet, this gets converted in the Haversine formula but could probably
+            // converted in seeding for a cleaner haversine if wanted, just will need to update that function
             modelBuilder.Entity<Airport>().HasData(
                 new Airport { AirportId = 1, AirportCode = "CLE", City = "Cleveland", State = "Ohio", Country = "USA", Latitude = 41.411667M, Longitude = -81.849722M, Elevation = 791 },
                 new Airport { AirportId = 2, AirportCode = "ATL", City = "Atlanta", State = "Georgia", Country = "USA", Latitude = 33.636667M, Longitude = -84.428056M, Elevation = 1027 },
@@ -43,6 +45,26 @@ namespace Air_3550.Repo
                 new Airport { AirportId = 8, AirportCode = "MIA", City = "Miami", State = "Florida", Country = "USA", Latitude = 25.793333M, Longitude = -80.290556M, Elevation = 9 },
                 new Airport { AirportId = 9, AirportCode = "SEA", City = "Seattle", State = "Washington", Country = "USA", Latitude = 47.448889M, Longitude = -122.309444M, Elevation = 433 },
                 new Airport { AirportId = 10, AirportCode = "BNA", City = "Nashville", State = "Tennessee", Country = "USA", Latitude = 36.126667M, Longitude = -86.681944M, Elevation = 599}
+            );
+
+            // Seeding Plane Model data
+            // note: MaxDistance is in units of kilometers, to work properly with our Haversine Function
+            // which results in a distance in meters, if we decide to adjust haversine we need to adjust these
+            modelBuilder.Entity<Plane>().HasData(
+                new Plane { PlaneId = 1, Model = "Boeing 737 MAX", MaxSeats = 230, MaxDistance = 6570},
+                new Plane { PlaneId = 2, Model = "Boeing 747", MaxSeats = 416, MaxDistance = 14815},
+                new Plane { PlaneId = 3, Model = "Boeing 757", MaxSeats = 199, MaxDistance = 6241},
+                new Plane { PlaneId = 4, Model = "Boeing 777", MaxSeats = 550, MaxDistance = 17395}
+            );
+
+            // Seeding Staff Accounts
+            // note: passwords are hashed with SHA-512 by some random website, probably should check it before doing more
+            // currently staff passwords are the exact same as their loginid
+            modelBuilder.Entity<User>().HasData(
+                new User { UserId = 1, LoginId = "marketing_manager", HashedPass = "360caebd9edb68609c0933bade3565350e59e284cc503ce61bf0eebd42fb7e5bd657a71ed1498225168757e7f1095920411cce27779e0c778ec52535deae2040", UserRole = Role.MARKETING_MANAGER },
+                new User { UserId = 2, LoginId = "load_engineer", HashedPass = "41ec260efa3aa054a91cc6cf9441e3652637f75946c8fa6c2e926f289e095f46e62e19a28e02fb0fc25dd047b1f04e6e03cf930d464b540c40c0045eb6b7e252", UserRole = Role.LOAD_ENGINEER },
+                new User { UserId = 3, LoginId = "accounting_manager", HashedPass = "ab8f196b4521a3aba1de420fe6ef552ce406d8551c3aef370f909cea85abbc77ca1d698ef31f659097eee16e365975047ff403df1aae6cc7ef54595c3ae4d172", UserRole = Role.ACCOUNTING_MANAGER },
+                new User { UserId = 4, LoginId = "flight_manager", HashedPass = "e2289f3f3a66a81f5ffb52dff1a09cd2ae91a39eb248230d2907084679c6bacdb4a880da7835ca72003d8c37e107cf91c5f9795678303754eba1be42039bff4d", UserRole = Role.FLIGHT_MANAGER }
             );
         }
     }
