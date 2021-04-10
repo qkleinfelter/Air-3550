c79c6ddcafe1f7a639df0c05e9b3eb3f9aeef56f
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-04-10 14:33:15 -0400

    update airport suggestion box

--- Air-3550/Air-3550/AirportSuggest.cs
+++ Air-3550/Air-3550/AirportSuggest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Air_3550.Repo;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Documents;
@@ -19,6 +21,8 @@ namespace Air_3550
         public AirportSuggest()
         {
             this.DefaultStyleKey = typeof(AirportSuggest);
+
+            this.Loaded += FillSuggestions;
         }
 
         public string Label
@@ -27,13 +31,15 @@ namespace Air_3550
             set => SetValue(LabelProperty, value);
         }
 
-        DependencyProperty LabelProperty = DependencyProperty.Register(
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register(
             nameof(Label),
             typeof(string),
             typeof(AirportSuggest),
             new PropertyMetadata(default(string), new PropertyChangedCallback(OnLabelChanged)));
 
         public bool HasLabelValue { get; set; }
+
+        public List<string> AirportNames = new List<string>();
 
         private static void OnLabelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -47,6 +53,48 @@ namespace Air_3550
             {
                 labelControl.HasLabelValue = true;
             }
+        }
+
+        private void AirportSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            var suitableItems = new List<string>();
+            var splitText = sender.Text.ToLower().Split(" ");
+            using (var db = new AirContext())
+            {
+                var airports = db.Airports;
+                foreach (var airport in airports)
+                {
+                    var found = splitText.All((key) =>
+                    {
+                        return airport.City.ToLower().Contains(key) || airport.AirportCode.ToLower().Contains(key);
+                    });
+                    if (found)
+                    {
+                        suitableItems.Add(airport.City + " (" + airport.AirportCode + ")");
+                    }
+                }
+                if (suitableItems.Count == 0)
+                {
+                    suitableItems.Add("No Results Found");
+                }
+                sender.ItemsSource = suitableItems;
+            }
+        }
+
+        private async void FillSuggestions(object sender, RoutedEventArgs args)
+        {
+            using (var db = new AirContext())
+            {
+                var airports = await db.Airports.ToListAsync();
+                AirportNames.AddRange(airports.Select(airport => airport.City + " (" + airport.AirportCode + ")"));
+            }
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var autoSuggestBox = GetTemplateChild("SuggestBox") as AutoSuggestBox;
+            autoSuggestBox.TextChanged += AirportSuggestBox_TextChanged;
         }
     }
 }
