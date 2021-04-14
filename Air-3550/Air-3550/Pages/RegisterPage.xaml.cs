7d997c5e521b1eb52b17c1e1c00b266b9f466539
GoldenWon <golden-james@att.net>
2021-04-14 13:25:05 -0400

    Added basic data validation for Create Account button.

--- Air-3550/Air-3550/Pages/RegisterPage.xaml.cs
+++ Air-3550/Air-3550/Pages/RegisterPage.xaml.cs
@@ -29,5 +29,87 @@ namespace Air_3550.Pages
         {
             Frame.Navigate(typeof(MainPage));
         }
+
+        private bool ValidateInput()
+        {
+            outputBlock.Text = "";
+           
+            if (string.IsNullOrWhiteSpace(NameInput.Text))
+            {
+                outputBlock.Text = "Please provide your full name.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(AddressInput.Text))
+            {
+                outputBlock.Text = "Please provide your address.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(CityInput.Text))
+            {
+                outputBlock.Text = "Please provide your city.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(StateInput.Text))
+            {
+                outputBlock.Text = "Please provide your state.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(ZipInput.Text))
+            {
+                outputBlock.Text = "Please provide your zip code.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(PhoneInput.Text))
+            {
+                outputBlock.Text = "Please provide your phone number.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(AgeInput.Text))
+            {
+                outputBlock.Text = "Please provide your age.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(CreditCardInput.Text))
+            {
+                outputBlock.Text = "Please provide your credit card.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(PasswordInput.Password))
+            {
+                outputBlock.Text = "Please provide a password.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(ConfirmPasswordInput.Password))
+            {
+                outputBlock.Text = "Please confirm your password.";
+                return false;
+            }
+
+            if (!(PasswordInput.Password.Equals(ConfirmPasswordInput.Password)))
+            {
+                outputBlock.Text = "Passords do not match.";
+                return false;
+            }
+
+            return true;
+        }
+
+        private void createAccountButton_Click(Object sender, RoutedEventArgs e)
+        {
+            // Input validation.
+            if (ValidateInput())
+            {
+                outputBlock.Text = "Thanks for doing your data part!";
+            }
+        }
     }
 }
