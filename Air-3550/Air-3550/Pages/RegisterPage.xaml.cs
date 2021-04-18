e438c9b0382522d0b11a0915f6efd6bf5b6630ce
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-04-18 14:01:26 -0400

    partially move register page stuff into a new control to be used for changing info too

--- Air-3550/Air-3550/Pages/RegisterPage.xaml.cs
+++ Air-3550/Air-3550/Pages/RegisterPage.xaml.cs
@@ -15,9 +15,7 @@ using Windows.Foundation.Collections;
 using Database.Utiltities;
 using Database.Models;
 using Air_3550.Models;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Air_3550.Controls;
 
 namespace Air_3550.Pages
 {
@@ -26,147 +24,12 @@ namespace Air_3550.Pages
         public RegisterPage()
         {
             this.InitializeComponent();
+            UserInfoControl.OnNavigateParentReady += UserInfoControl_OnNavigateParentReady;
         }
 
-        private void backButton_Click(Object sender, RoutedEventArgs e)
+        private void UserInfoControl_OnNavigateParentReady(object source, EventArgs e)
         {
             Frame.Navigate(typeof(MainPage));
-        }
-
-        private bool ValidateInput()
-        {
-            outputBlock.Text = "";
-           
-            if (string.IsNullOrWhiteSpace(NameInput.Text))
-            {
-                outputBlock.Text = "Please provide your full name.";
-                return false;
-            }
-
-            if (string.IsNullOrWhiteSpace(AddressInput.Text))
-            {
-                outputBlock.Text = "Please provide your address.";
-                return false;
-            }
-
-            if (string.IsNullOrWhiteSpace(CityInput.Text))
-            {
-                outputBlock.Text = "Please provide your city.";
-                return false;
-            }
-
-            if (string.IsNullOrWhiteSpace(StateInput.Text))
-            {
-                outputBlock.Text = "Please provide your state.";
-                return false;
-            }
-
-            if (string.IsNullOrWhiteSpace(ZipInput.Text))
-            {
-                outputBlock.Text = "Please provide your zip code.";
-                return false;
-            }
-
-            if (string.IsNullOrWhiteSpace(PhoneInput.Text))
-            {
-                outputBlock.Text = "Please provide your phone number.";
-                return false;
-            }
-
-            if (string.IsNullOrWhiteSpace(AgeInput.Text))
-            {
-                outputBlock.Text = "Please provide your age.";
-                return false;
-            }
-
-            if (string.IsNullOrWhiteSpace(CreditCardInput.Text))
-            {
-                outputBlock.Text = "Please provide your credit card.";
-                return false;
-            }
-
-            if (string.IsNullOrWhiteSpace(PasswordInput.Password))
-            {
-                outputBlock.Text = "Please provide a password.";
-                return false;
-            }
-
-            if (string.IsNullOrWhiteSpace(ConfirmPasswordInput.Password))
-            {
-                outputBlock.Text = "Please confirm your password.";
-                return false;
-            }
-
-            if (!(PasswordInput.Password.Equals(ConfirmPasswordInput.Password)))
-            {
-                outputBlock.Text = "Passords do not match.";
-                return false;
-            }
-
-            return true;
-        }
-
-        private int MakeUserID()
-        {
-            // Make 6-digit random int without leading 0s
-            // Instantiate random number generator using system-supplied value as seed.
-            var rand = new Random();
-
-            int userID;
-            while (true)
-            {
-                // Random 6-digit number, .next is inclusive on minimum & exclusive on max
-                userID = rand.Next(100000, 1000000);
-                if (!UserUtilities.LoginIDExists(userID.ToString()))
-                {
-                    // If the login id doesn't exist, then break out of the while loop and return
-                    break;
-                }
-            }
-
-            return userID;
-        }
-
-        private void createAccountButton_Click(Object sender, RoutedEventArgs e)
-        {
-            // Input validation.
-            if (ValidateInput())
-            {
-                outputBlock.Text = "";
-
-                // Get Random UserID
-
-                //printing for now to test, bout to head to work...
-                int userID = MakeUserID();
-
-                CustomerInfo customerInfo = new()
-                {
-                    Name = NameInput.Text,
-                    Address = AddressInput.Text,
-                    City = CityInput.Text,
-                    State = StateInput.Text,
-                    Zip = ZipInput.Text,
-                    PhoneNumber = PhoneInput.Text,
-                    Age = (int) AgeInput.Value,
-                    CreditCardNumber = CreditCardInput.Text
-                };
-
-                User user = new()
-                {
-                    LoginId = userID.ToString(),
-                    HashedPass = PasswordHandler.HashPassword(PasswordInput.Password),
-                    UserRole = Role.CUSTOMER,
-                    CustInfo = customerInfo
-                };
-
-                UserUtilities.AddUserToDB(user);
-
-                outputInfo.Title = "Account Creation Successful!";
-                outputInfo.Message = $"Your Login ID is: {userID}, please remember it for future logins!";
-                outputInfo.Severity = InfoBarSeverity.Success;
-                outputInfo.IsOpen = true;
-
-            }
         }
     }
 }
