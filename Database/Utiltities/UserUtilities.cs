960383d11b3ee5aa7ea5f02ca706f704c74ff9f4
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-04-19 16:13:02 -0400

    some small user utilities

--- Database/Utiltities/UserUtilities.cs
+++ Database/Utiltities/UserUtilities.cs
@@ -1,5 +1,6 @@
 ﻿using Air_3550.Models;
 using Air_3550.Repo;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +19,28 @@ namespace Database.Utiltities
         public static void AwardCredit(User user, int amount)
         {
             using var db = new AirContext();
-            var customerInfo = db.Users.Where(dbuser => dbuser.UserId == user.UserId).FirstOrDefault().CustInfo;
+            var customerInfo = db.Users.Include(user => user.CustInfo).Where(dbuser => dbuser.UserId == user.UserId).FirstOrDefault().CustInfo;
             if (customerInfo != null)
                 customerInfo.CreditBalance += amount;
+        }
+
+        public static void AwardPoints(User user, int amount)
+        {
+            using var db = new AirContext();
+            var customerInfo = db.Users.Include(user => user.CustInfo).Where(dbuser => dbuser.UserId == user.UserId).FirstOrDefault().CustInfo;
+            if (customerInfo != null)
+                customerInfo.PointsAvailable += amount;
+        }
+
+        public static void UsePoints(User user, int amount)
+        {
+            using var db = new AirContext();
+            var customerInfo = db.Users.Include(user => user.CustInfo).Where(dbuser => dbuser.UserId == user.UserId).FirstOrDefault().CustInfo;
+            if (customerInfo != null)
+            {
+                customerInfo.PointsUsed += amount;
+                customerInfo.PointsAvailable -= amount;
+            }
         }
 
         public static bool LoginIDExists(string loginId)
