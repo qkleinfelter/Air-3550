82e51bbb7cefc2204aae7fe054bfcb40e35ebdf1
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-04-19 16:32:47 -0400

    ticket utils base class

---
+++ Database/Utiltities/TicketUtilities.cs
@@ -0,0 +1,22 @@
+﻿using Air_3550.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Utiltities
+{
+    public class TicketUtilities
+    {
+        public static void PurchaseFlightTicket(User user, Ticket ticket)
+        {
+
+        }
+
+        public static void CancelFlightTicket(Ticket ticket)
+        {
+            
+        }
+    }
+}
