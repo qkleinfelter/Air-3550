79e8cb56c7a6c8eb53029886332a7b0f0d515023
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-04-23 17:21:32 -0400

    more progress on checkout

--- Air-3550/Air-3550/Pages/CheckoutPage.xaml.cs
+++ Air-3550/Air-3550/Pages/CheckoutPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Database.Utiltities;
+﻿using Air_3550.Models;
+using Database.Utiltities;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -29,6 +30,8 @@ namespace Air_3550.Pages
             }
         }
         private Parameters passedInParams;
+        private int leavingPathCost = 0;
+        private int returningPathCost = 0;
         public CheckoutPage()
         {
             this.InitializeComponent();
@@ -42,6 +45,45 @@ namespace Air_3550.Pages
             outputInfo.Message = $"{passedInParams.leavingPath.DepartureTime} -> {passedInParams.leavingPath.ArrivalTime} & {passedInParams.returningPath.DepartureTime} -> {passedInParams.returningPath.ArrivalTime}";
             outputInfo.Severity = InfoBarSeverity.Informational;
             outputInfo.IsOpen = true;
+            leavingPathCost = int.Parse(passedInParams.leavingPath.Price.Substring(1));
+            returningPathCost = int.Parse(passedInParams.returningPath.Price.Substring(1));
+            DisplayInfo();
+        }
+        
+        private void DisplayInfo()
+        {
+            int numFlights = passedInParams.leavingPath.flights.Count;
+            Airport origin = passedInParams.leavingPath.flights[0].Origin;
+            Airport dest = passedInParams.leavingPath.flights[numFlights - 1].Destination;
+            departInfo.Text = "Departing Flight Information:";
+            departInfo.Text += $"\nFrom {origin.City} to {dest.City}";
+            departInfo.Text += $"\nTotal Flight Duration: {passedInParams.leavingPath.FormattedDuration}";
+            departInfo.Text += $"\nWith stops: {passedInParams.leavingPath.FormattedStops}";
+            departInfo.Text += $"\nPrice: ${leavingPathCost}";
+
+            returnInfo.Text = "Returning Flight Information:";
+            returnInfo.Text += $"\nFrom {dest.City} to {origin.City}";
+            returnInfo.Text += $"\nTotal Flight Duration: {passedInParams.returningPath.FormattedDuration}";
+            returnInfo.Text += $"\nWith stops: {passedInParams.returningPath.FormattedStops}";
+            returnInfo.Text += $"\nPrice: ${returningPathCost}";
+
+            summaryInfo.Text = "Summary:";
+            summaryInfo.Text += $"Total Cost: {leavingPathCost + returningPathCost}";
+        }
+
+        private void useCredit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void usePoints_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void useCreditCard_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
