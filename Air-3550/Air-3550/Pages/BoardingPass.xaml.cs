22454ea1cf3e8079e9809966d013aa92e6e22d6f
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-04-25 19:06:12 -0400

    fixed build errors

--- Air-3550/Air-3550/Pages/BoardingPass.xaml.cs
+++ Air-3550/Air-3550/Pages/BoardingPass.xaml.cs
@@ -17,14 +17,8 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Air_3550.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class BoardingPass : Page
     {
         public BoardingPass()
@@ -47,7 +41,7 @@ namespace Air_3550.Pages
             Destination.Text = Btrip.Destination.AirportCode;
         }
 
-        private void BackButton_Click(object sender, RoutedEventArgs e)
+        private void backButton_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(AccountPage));
         }
