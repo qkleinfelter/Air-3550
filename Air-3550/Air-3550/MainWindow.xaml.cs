d16864cf73e357cf0a86c18715f8166f97490cba
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2021-04-27 18:01:04 -0400

    add fancy header comment

--- Air-3550/Air-3550/MainWindow.xaml.cs
+++ Air-3550/Air-3550/MainWindow.xaml.cs
@@ -1,28 +1,19 @@
-﻿using Air_3550.Repo;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿// MainWindow.xaml.cs - Air 3550 Project
+//
+// This program acts as a flight reservation system for a new airline - called Air-3550,
+// which allows customers to book and manage trips, which contain many flights, as well as enabling various staff members
+// to update the available flights and view statistics about them individually or as a whole.
+//
+// Authors:     Quinn Kleinfelter, James Golden, & Edward Walsh
+// Class:       EECS 3550-001 Software Engineering, Spring 2021
+// Instructor:  Dr. Thomas
+// Date:        April 28, 2021
+// Copyright:   Copyright 2021 by Quinn Kleinfelter, James Golden, & Edward Walsh. All rights reserved.
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI.Xaml;
 
 namespace Air_3550
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
--- Air-3550/Air-3550/Pages/FlightDisplayPage.xaml.cs
+++ Air-3550/Air-3550/Pages/FlightDisplayPage.xaml.cs
@@ -1,4 +1,16 @@
-﻿using Air_3550.Models;
+﻿// FlightDisplayPage.xaml.cs - Air 3550 Project
+//
+// This program acts as a flight reservation system for a new airline - called Air-3550,
+// which allows customers to book and manage trips, which contain many flights, as well as enabling various staff members
+// to update the available flights and view statistics about them individually or as a whole.
+//
+// Authors:     Quinn Kleinfelter, James Golden, & Edward Walsh
+// Class:       EECS 3550-001 Software Engineering, Spring 2021
+// Instructor:  Dr. Thomas
+// Date:        April 28, 2021
+// Copyright:   Copyright 2021 by Quinn Kleinfelter, James Golden, & Edward Walsh. All rights reserved.
+
+using Air_3550.Models;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
